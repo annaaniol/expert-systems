b7951e5b35b9ddfeb4f5cc6fc1831d619ec84565
Anna Anioł <15068386+annaaniol@users.noreply.github.com>
2018-11-03 12:28:09 +0100

    Add simple activities

--- holiday.pl
+++ holiday.pl
@@ -2,11 +2,14 @@
 :- discontiguous menuask/3.
 :- discontiguous ask/2.
 
+/* ------- Knowledge base ------- */
+
 offer(porto) :- ( location_type(seaside);
                   location_type(city) ),
                 ( trip_time(summer);
                   trip_time(autumn) ),
-                wine_tasting(yes).
+                ( activity_type(wine_tasting);
+                  activity_type(swimming) ).
 
 offer(london) :- location_type(city),
                  ( trip_time(spring);
@@ -14,12 +17,47 @@ offer(london) :- location_type(city),
                    trip_time(autumn);
                    trip_time(winter) ).
 
-location_type(Q) :- menuask(location_type, Q, [city, seaside]).
-trip_time(Q) :- menuask(trip_time, Q, [spring, summer, autumn, winter]).
-wine_tasting(Q) :- ask(wine_tasting, Q).
+offer(cortina_dAmpezzo) :- location_type(mountains),
+                           trip_time(winter).
 
-find :- offer(X), !,
-        nl, format('Think out of our trip to ~w', X), nl.
+
+/* ------- Rules ------- */
+
+/*
+   Optional:
+   location_type(Q) :- menuask(location_type, Q, [city, seaside]).
+*/
+location_type(city) :- sightseeing(yes).
+location_type(seaside) :- sea(yes).
+location_type(mountains) :- mountains(yes),
+                            ( activity_type(skiing);
+                              activity_type(snowboarding) ).
+
+trip_time(Q) :- menuask(trip_time, Q, [spring, summer, autumn, winter]).
+
+activity_type(skiing) :- trip_time(winter),
+                         skiing(yes).
+
+activity_type(snowboarding) :- trip_time(winter),
+                               snowboarding(yes).
+
+activity_type(swimming) :- swimming(yes).
+
+activity_type(wine_tasting) :- wine_tasting(yes).
+
+sport(Q) :- ask(sport, Q).
+skiing(Q) :- sport(yes), ask(skiing, Q).
+snowboarding(Q) :- sport(yes), ask(snowboarding, Q).
+swimming(Q) :- sport(yes), ask(swimming, Q).
+
+wine_tasting(Q) :- ask(wine_tasting, Q).
+sightseeing(Q) :- ask(sightseeing, Q).
+
+sea(Q) :- ask(sea, Q).
+mountains(Q) :- ask(mountains, Q).
+
+
+/* ------- Asking logic ------- */
 
 /* Do not ask if it has been already positively answered */
 ask(Key, Val) :- known(yes, Key, Val), !.
@@ -46,6 +84,9 @@ menuask(Key, Val, List) :-
                             asserta(known(yes, Key, Ans)),
                             Ans == Val.
 
+
+/* ------- Processing answers ------- */
+
 /* Check if a picked element is present in the list */
 check_val(Ans, _, _, List) :- member(Ans, List), !.
 
@@ -53,3 +94,11 @@ check_val(Ans, _, _, List) :- member(Ans, List), !.
 check_val(Ans, Key, Val, List) :-
                             write(Ans), write(' is not a known answer, please try again.'), nl,
                             menuask(Key, Val, List).
+
+
+/* ------- Executives ------- */
+
+find :- offer(X), !,
+        nl, format('Think out of our trip to ~w', X), nl.
+
+clear :- retractall(known(_,_,_)).
