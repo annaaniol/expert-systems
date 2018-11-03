0e958009101d78f763ad04de577a155b7801db91
Anna Anioł <15068386+annaaniol@users.noreply.github.com>
2018-11-03 09:49:16 +0100

    Add yes/no choices

--- holiday.pl
+++ holiday.pl
@@ -1,10 +1,12 @@
 :- dynamic(known/3).
 :- discontiguous menuask/3.
+:- discontiguous ask/2.
 
 offer(porto) :- ( location_type(seaside);
                   location_type(city) ),
                 ( trip_time(summer);
-                  trip_time(autumn) ).
+                  trip_time(autumn) ),
+                wine_tasting(yes).
 
 offer(london) :- location_type(city),
                  ( trip_time(spring);
@@ -14,18 +16,27 @@ offer(london) :- location_type(city),
 
 location_type(Q) :- menuask(location_type, Q, [city, seaside]).
 trip_time(Q) :- menuask(trip_time, Q, [spring, summer, autumn, winter]).
+wine_tasting(Q) :- ask(wine_tasting, Q).
 
 find :- offer(X), !,
         nl, format('Think out of our trip to ~w', X), nl.
 
 /* Do not ask if it has been already positively answered */
+ask(Key, Val) :- known(yes, Key, Val), !.
 menuask(Key, Val, _) :- known(yes, Key, Val), !.
 
 /* Do not ask if it has been already negatively answered */
+ask(Key, Val) :- known(_, Key, Val), !, fail.
 menuask(Key, Val, _) :- known(_, Key, Val), !, fail.
 
 /* Do not ask if it has been already answered in a different way */
 menuask(Key, Val, _) :- known(yes, Key, OtherVal),  OtherVal\== Val, !, fail.
+
+/* Finally, ask */
+ask(Key, Val) :-
+                 nl, format('Do you want ~w ?', [Key]), nl,
+                 read(Ans),
+                 asserta(known(Ans, Key, Val)), Ans == yes.
 
 menuask(Key, Val, List) :-
                             nl, format('Please type your preferred ~w ?', [Key]), nl,
